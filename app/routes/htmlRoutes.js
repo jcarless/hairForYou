5bfb6ad514e0b49436f0a07f92d4ea990a7cf150
Jerome Carless <carless.jerome@gmail.com>
2016-05-14 19:37:18 -0400

    created coming soon page

---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = function(app){
+
+    app.get('/index', function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/index.html'));
+
+    });
+
+    app.use(function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/index.html'));
+    });
+
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+// initial requires
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// setup the express app
+var app = express();
+
+// use body-parser to help express handle requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// // set up handlebars for express
+// var exphbs = require('express-handlebars');
+// app.engine('handlebars', exphbs({
+//   defaultLayout: 'main',
+//     extname: '.handlebars',
+//     layoutsDir: 'app/views/layouts'
+// }));
+//
+// app.set('view engine', 'handlebars');
+// app.set('views', __dirname + '/app/views');
+
+// load the static files
+var staticContentFolder = __dirname + '/app/public';
+app.use(express.static(staticContentFolder));
+
+
+// require the html path
+require("./app/routes/htmlRoutes.js")(app)
+
+// start the server
+var PORT = process.env.PORT || 1988;
+app.listen(PORT, function(){
+	console.log('Find the magic at port: ' + PORT);
+})
